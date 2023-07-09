8f27b8d10e039191df9b67892d4daa3918e423a7
Ezra M <124730310+Ez-Megalodon@users.noreply.github.com>
2023-07-09 09:51:09 -0500

    initialization

---
+++ CurrencyConversion.java
@@ -0,0 +1,31 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CurrencyConversion {
+    public static NumberFormat usa = NumberFormat.getCurrencyInstance(Locale.US);
+    public static NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+    public static NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+    public static String indiaConvert(double num) {
+        DecimalFormatSymbols sep = new DecimalFormatSymbols(Locale.ENGLISH);
+        sep.setGroupingSeparator(',');
+        sep.setDecimalSeparator('.');
+
+        DecimalFormat decimalFormat = new DecimalFormat("#,##0.00", sep);
+
+        return "Rs." + decimalFormat.format(num);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+
+        System.out.println("US: " + usa.format(payment));
+        System.out.println("India: " + indiaConvert(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
---
+++ DayOfWeek.java
@@ -0,0 +1,15 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DayOfWeek {
+    public static String findDay(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, day); // Note: month is zero-based in Calendar, so subtract 1
+
+        Date date = cal.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+        String dayOfWeek = sdf.format(date);
+        return dayOfWeek.toUpperCase();
+    }
+}
