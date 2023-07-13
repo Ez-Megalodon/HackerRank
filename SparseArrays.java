fe6437b384275308954bbd7df9a504f10e103cf0
Ezra M <124730310+Ez-Megalodon@users.noreply.github.com>
2023-07-13 15:01:49 -0400

    finished SparseArrays challenge.

---
+++ SparseArrays.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SparseArrays {
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+        List<Integer> nums = new ArrayList<>();
+        int total = 0;
+        for (int i = 0; i < queries.size(); i++){
+            total = 0;
+            for (int k = 0; k < strings.size(); k++){
+                if (strings.get(k).equals(queries.get(i))){
+                    total++;
+                }
+            }
+            nums.add(total);
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        strings.add("abc");
+        strings.add("bc");
+        strings.add("abc");
+
+        List<String> queries = new ArrayList<>();
+        queries.add("abc");
+        queries.add("bc");
+        queries.add("b");
+
+        matchingStrings(strings, queries);
+    }
+
+}
