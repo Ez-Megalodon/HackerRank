4b09264b1d7dabc6f248431966dd51013ef8b77a
Ezra M <124730310+Ez-Megalodon@users.noreply.github.com>
2023-07-21 21:21:33 -0500

    finished Pangrams challenge.

---
+++ Pangrams.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pangrams {
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String pangrams(String s) {
+        List<Character> alphabetList = new ArrayList<>();
+        s = s.toLowerCase();
+        char[] stringArray = s.toCharArray();
+
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            alphabetList.add(letter);
+        }
+
+        for (int i = 0; i < s.length(); i++){
+            if (alphabetList.contains(stringArray[i])){
+                alphabetList.remove((Character)stringArray[i]);
+            }
+        }
+
+        if (alphabetList.size() > 0){
+            return "not pangram";
+        } else {
+            return "pangram";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pangrams("The quick brown fox jumps over a lazy dog"));
+    }
+}
