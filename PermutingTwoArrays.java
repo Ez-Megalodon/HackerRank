acfb8b567949aa574bd75f1a0b1a1a36a7412132
Ezra M <124730310+Ez-Megalodon@users.noreply.github.com>
2023-07-22 21:17:48 -0500

    finished PermutingTwoArrays challenge.

---
+++ PermutingTwoArrays.java
@@ -0,0 +1,21 @@
+import java.util.Collections;
+import java.util.List;
+
+public class PermutingTwoArrays {
+    public static String twoArrays(int k, List<Integer> A, List<Integer> B) {
+        if (A.size() != B.size()) {
+            return "NO";
+        }
+
+        Collections.sort(A);
+        Collections.sort(B, Collections.reverseOrder());
+
+        for (int i = 0; i < A.size(); i++) {
+            if (A.get(i) + B.get(i) < k) {
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+
+}
