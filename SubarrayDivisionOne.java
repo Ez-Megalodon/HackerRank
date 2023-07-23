30f0d747bcb1e5ed4c66a1f94250ef0b3a4cf88b
Ezra M <124730310+Ez-Megalodon@users.noreply.github.com>
2023-07-23 08:37:29 -0500

    finished SubarrayDivisionOne challenge.

---
+++ SubarrayDivisionOne.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class SubarrayDivisionOne {
+    public static int birthday(List<Integer> s, int d, int m) {
+        int totalWays = 0;
+        int sum = 0;
+
+        for (int i = 0; i < m; i++){
+            sum += s.get(i);
+        }
+
+        for (int j = m; j < s.size(); j++){
+            if (sum == d){
+                totalWays++;
+            }
+            sum = sum - s.get(j - m) + s.get(j);
+        }
+
+        if (sum == d){
+            totalWays++;
+        }
+
+        return totalWays;
+    }
+
+
+}
