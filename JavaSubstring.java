292d2300ab4fdf6eef1437069a5ea0b7e8651c63
Ezra M <124730310+Ez-Megalodon@users.noreply.github.com>
2023-08-02 14:13:31 -0500

    Finished Substring challenge

---
+++ JavaSubstring.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class JavaSubstring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        in.close();
+
+        if(start >= 0 && end <= s.length() && start <= end){
+            String result = s.substring(start, end);
+            System.out.println(result);
+        }
+    }
+}
