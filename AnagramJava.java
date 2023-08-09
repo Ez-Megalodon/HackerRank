fe07fe8d81b2763dc945fade056928fea143a757
Ezra M <124730310+Ez-Megalodon@users.noreply.github.com>
2023-08-09 09:07:02 -0500

    Finished Java Anagram challenge

---
+++ AnagramJava.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class AnagramJava {
+    static boolean isAnagram(String a, String b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        int[] A = new int[26];
+        int[] B = new int[26];
+
+        for (char ch : a.toCharArray()) {
+            A[ch - 'a']++;
+        }
+        for (char ch : b.toCharArray()) {
+            B[ch - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (A[i] != B[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+
+        if (isAnagram(a, b)) {
+            System.out.println("Anagrams");
+        } else {
+            System.out.println("Not Anagrams");
+        }
+    }
+}
