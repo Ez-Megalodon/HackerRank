d3b12ac7c01a1a4c347481ef779a2a686929a36b
Ezra M <124730310+Ez-Megalodon@users.noreply.github.com>
2023-08-11 18:12:36 -0500

    Finished Valid Username Regular Expression challenge

---
+++ ValidUsernameRegularExpression.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ValidUsernameRegularExpression {
+    public static final String regularExpression = "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = Integer.parseInt(scan.nextLine());
+
+        while (a-- != 0) {
+            String username = scan.nextLine();
+
+            if (username.matches(ValidUsernameRegularExpression.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
