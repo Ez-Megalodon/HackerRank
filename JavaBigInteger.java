8f77e9d2d09e786b3032eca57a9c149bd3f1a884
Ezra M <124730310+Ez-Megalodon@users.noreply.github.com>
2023-08-15 23:59:37 -0500

    Finished Java Big Int challenge

---
+++ JavaBigInteger.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class JavaBigInteger {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.nextLine().trim();
+        String b = scanner.nextLine().trim();
+
+        BigInteger num1 = new BigInteger(a);
+        BigInteger num2 = new BigInteger(b);
+
+        BigInteger sum = num1.add(num2);
+        BigInteger product = num1.multiply(num2);
+
+        System.out.println(sum);
+        System.out.println(product);
+    }
+}
